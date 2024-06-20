f2c180e334ace87b3b25844a35ce311fbe311725
dominickasanga <dominickasanga@gmail.com>
2024-06-20 11:46:44 +0200

    Ignore app/mahis directory

--- app/mahis/assets/web-987d6fb4.js
+++
@@ -1,1 +0,0 @@
-import{W as n,U as t}from"./index-6954243b.js";class l extends n{async write(e){if(e.url)return t.expandPath(e.url).then(r=>{document.location=r});throw new TypeError("Undefined url")}async discover(){return{devices:["webPrinter"]}}}export{l as LabelPrinterWeb};
--- app/mahis/assets/web-legacy-17bc29c9.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index-legacy-d271d5bd.js"],(function(e,r){"use strict";var t,n;return{setters:[e=>{t=e.W,n=e.U}],execute:function(){e("LabelPrinterWeb",class extends t{async write(e){if(e.url)return n.expandPath(e.url).then((e=>{document.location=e}));throw new TypeError("Undefined url")}async discover(){return{devices:["webPrinter"]}}})}}}));
